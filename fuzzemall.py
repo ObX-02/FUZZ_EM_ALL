fadf9f96679cc49b2f528698932c8333a9a069d2
obx-02 <ObX-02@users.noreply.github.com>
2026-07-17 12:56:41 +0500

    Status_code_Extraction

--- fuzzemall.py
+++ fuzzemall.py
@@ -1,3 +1,4 @@
+import requests
 print("========================================")
 print("            FUZZ 'EM  ALL !!            ")
 print("========================================")
@@ -6,16 +7,31 @@ if not Target.startswith(("http://","https://")):
     Target = "https://" + Target
     
 print(f"Target Selected: {Target}")
-count = 0
 with open("common.txt","r")as f:
     print("========================================")
     print("Reading Wordlist......")
     print("========================================")
 
-
+    count = 0
     for data in f:
         count+=1
-        print(f"{count:2} {Target}{data.strip()}")
+        full_url = Target + data.strip()
+        print(f"{count:2} {full_url}") 
+print("========================================")
+print("Checking Target Reponse.......")
+print("========================================")
+f.close()
+count = 0
+with open("common.txt","r")as f:
+    for data in f:
+        count+=1
+        full_url = Target + data.strip()
+        response = requests.get(full_url)
+        print(f"{count:2} {full_url}        {response.status_code}") 
+        
+        
+  
+
     
 
         
