277cd0901de3992bcd6550faf7409bc0285e78e2
obx-02 <ObX-02@users.noreply.github.com>
2026-07-17 12:02:02 +0500

    Added wordlist reader

---
+++ fuzzemall.py
@@ -0,0 +1,21 @@
+print("========================================")
+print("            FUZZ 'EM  ALL !!            ")
+print("========================================")
+Target = input("Enter Target's URL: ").strip()
+if not Target.startswith(("http://","https://")):
+    Target = "https://" + Target
+    
+print(f"Target Selected: {Target}")
+count = 0
+with open("common.txt","r")as f:
+    print("========================================")
+    print("reading Wordlist......")
+    print("========================================")
+
+
+    for data in f:
+        count+=1
+        print(f"{count:2} {data.strip()}")
+    
+
+        
