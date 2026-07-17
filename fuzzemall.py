9b94d8cab6dbbb6209d049ea400837f4c9164fa4
obx-02 <ObX-02@users.noreply.github.com>
2026-07-18 00:17:28 +0500

    use_colorama_library

--- fuzzemall.py
+++ fuzzemall.py
@@ -1,4 +1,5 @@
 import requests
+from colorama import Fore, Style
 print("========================================")
 print("            FUZZ 'EM  ALL !!            ")
 print("========================================")
@@ -29,28 +30,45 @@ with open("common.txt","r")as f:
 }
     found = 0
     not_found = 0
+    errors = 0
+    ok = 0
+    forbidden = 0
+    redirect = 0
+    total_requests = 0
     for data in f:
         count+=1
         full_url = Target + data.strip().lstrip("/")
+        total_requests+=1
         try:
             response = requests.get(full_url, headers= headers, timeout = 3)
             size = len(response.content)
-            if response.status_code != 404:
-                found+= 1
+            if response.status_code == 200:
+                ok+= 1
+                print(Fore.GREEN + f"{count:2} {full_url}        {response.status_code}        {size} Bytes")
+            elif response.status_code == 304:
+                forbiddent+=1
+                print(Fore.YELLOW + f"{count:2} {full_url}        {response.status_code}        {size} Bytes")
+            elif response.status_code == 301:
+                redirect+=1
                 print(f"{count:2} {full_url}        {response.status_code}        {size} Bytes") 
             else:
                 not_found+=1  
                  
 
         except requests.exceptions.RequestException:
-            print(f"Connection Failed: {full_url}")
+            print(Fore.RED + f"Connection Failed: {full_url}")
+            errors+=1
             
 
 print("========================================")
 print("             Scan Finished              ")
 print("========================================")
-print(f"found: {found}")
-print(f"not found: {not_found}") 
+print(f"Total Requests: {total_requests}")
+print(f"200 OK: {ok}")
+print(f"304 forbidden: {forbidden}")
+print(f"301 Redirect: {redirect}")
+print(f"404 not found: {not_found}") 
+print(f"Errors: {errors}" )
 print("========================================")
 
 
