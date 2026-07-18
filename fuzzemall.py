f13b82bd6b0d8cddb324476e98f4a9c0f47d49d8
obx-02 <ObX-02@users.noreply.github.com>
2026-07-18 16:58:17 +0500

    fuzzEmAll

--- fuzzemall.py
+++ fuzzemall.py
@@ -21,55 +21,72 @@ with open("common.txt","r")as f:
         full_url = Target + data.strip().lstrip("/")
         print(f"{count:2} {full_url}") 
 print("========================================")
-print("          Checking Target Reponse.......")
+print("          Scaning Result                ")
 print("========================================")
 count = 0
-with open("common.txt","r")as f:
-    headers = {
-    "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36" 
-}
-    found = 0
-    not_found = 0
-    errors = 0
-    ok = 0
-    forbidden = 0
-    redirect = 0
-    total_requests = 0
-    for data in f:
-        count+=1
-        full_url = Target + data.strip().lstrip("/")
-        total_requests+=1
-        try:
-            response = requests.get(full_url, headers= headers, timeout = 3)
-            size = len(response.content)
-            if response.status_code == 200:
-                ok+= 1
-                print(Fore.GREEN + f"{count:2} {full_url}        {response.status_code}        {size} Bytes")
-            elif response.status_code == 304:
-                forbiddent+=1
-                print(Fore.YELLOW + f"{count:2} {full_url}        {response.status_code}        {size} Bytes")
-            elif response.status_code == 301:
-                redirect+=1
-                print(f"{count:2} {full_url}        {response.status_code}        {size} Bytes") 
-            else:
-                not_found+=1  
-                 
 
-        except requests.exceptions.RequestException:
-            print(Fore.RED + f"Connection Failed: {full_url}")
-            errors+=1
+with open("common.txt","r")as f:
+    with open("result.txt","w",encoding = "utf-8")as result:
+
+        headers = {
+        "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/138.0.0.0 Safari/537.36" 
+        }
+        found = 0
+        not_found = 0
+        errors = 0
+        ok = 0
+        forbidden = 0
+        redirect = 0
+        server_error =0
+        total_requests = 0
+        for data in f:
+            count+=1
+            full_url = Target + data.strip().lstrip("/")
+            total_requests+=1
+            try:
+                response = requests.get(full_url, headers= headers, timeout = 10)
+                size = len(response.content)
+                line = f"{count:2} {full_url}        {response.status_code}        {size} Bytes"
+                if response.status_code == 200:
+                    ok+= 1
+                    print(Fore.GREEN + line)
+                    result.write(line +"\n")
+                elif response.status_code == 304:
+                    forbiddent+=1
+                    print(Fore.YELLOW + line)
+                    result.write(line+ "\n")
+                elif response.status_code == 301:
+                    redirect+=1
+                    print(line)
+                    result.write(line+"\n")
+                elif response.status_code == 500:
+                    server_error+= 1
+                    result.write(line+ "\n")
+                else:
+                    not_found+=1  
+                    
+
+            except requests.exceptions.RequestException:
+                print(Fore.RED + f"Connection Failed: {full_url}")
+                errors+=1
+                result.write(line+ "\n")
             
 
 print("========================================")
 print("             Scan Finished              ")
 print("========================================")
 print(f"Total Requests: {total_requests}")
-print(f"200 OK: {ok}")
-print(f"304 forbidden: {forbidden}")
-print(f"301 Redirect: {redirect}")
-print(f"404 not found: {not_found}") 
+print(f"200 = OK: {ok}")
+print(f"403 = Forbidden: {forbidden}")
+print(f"301 = Moved Permanently (Redirect): {redirect}")
+print(f"404 = Not found: {not_found}")
+print(f"500 = Internal Server Error: {server_error}") 
 print(f"Errors: {errors}" )
 print("========================================")
+print("========================================")
+print("Also Data is stored in result.txt file")
+print("========================================")
+
 
 
   
